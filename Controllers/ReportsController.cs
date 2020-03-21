196e34ff0c576ab5b6f514f74639fea0c48c9927
stevo9510 <stevo9510@gmail.com>
2020-03-21 06:27:50 -0400

    New controller in works for displaying reports.

---
+++ Controllers/ReportsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MovieDbLite.MVC.Models;
+using System.Threading.Tasks;
+
+namespace MovieDbLite.MVC.Controllers
+{
+    public class ReportsController : Controller
+    {
+        private readonly MovieDbLiteContext _context;
+
+        public ReportsController(MovieDbLiteContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Reports
+        public async Task<IActionResult> Index()
+        {
+            var winnerInfo = _context.VwAwardWinnerInfo;
+            return View(await winnerInfo.ToListAsync());
+        }
+
+        // GET: Reports/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+    }
+}
