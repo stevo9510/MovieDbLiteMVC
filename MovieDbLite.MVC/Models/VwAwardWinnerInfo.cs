511141a5d41c87e166c58ae795708edb073f3590
stevo9510 <stevo9510@gmail.com>
2020-04-18 17:36:58 -0400

    Updated headers for each page, and added drop down links to nav bar for main pages

--- MovieDbLite.MVC/Models/VwAwardWinnerInfo.cs
+++ MovieDbLite.MVC/Models/VwAwardWinnerInfo.cs
@@ -1,18 +1,25 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace MovieDbLite.MVC.Models
 {
     public partial class VwAwardWinnerInfo
     {
         public int AwardId { get; set; }
+        [Display(Name = "Award")]
         public string AwardName { get; set; }
         public short AwardShowId { get; set; }
+        [Display(Name = "Show")]
         public string ShowName { get; set; }
+        [Display(Name = "Year")]
         public short Year { get; set; }
         public long MovieId { get; set; }
+        [Display(Name = "Movie")]
         public string Title { get; set; }
         public long FilmMemberId { get; set; }
+        [Display(Name = "Name")]
         public string PreferredFullName { get; set; }
+        [Display(Name = "Award Date"), DataType(DataType.Date)]
         public DateTime DateHosted { get; set; }
     }
 }
