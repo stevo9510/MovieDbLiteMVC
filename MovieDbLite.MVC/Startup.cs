ffef87a657c671ed4d84b159c3deab9188c99fc7
stevo9510 <stevo9510@gmail.com>
2020-05-24 03:44:14 -0400

    Added docker and status pages

--- MovieDbLite.MVC/Startup.cs
+++ MovieDbLite.MVC/Startup.cs
@@ -85,6 +85,8 @@ namespace MovieDbLite.MVC
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            app.UseStatusCodePages();
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
